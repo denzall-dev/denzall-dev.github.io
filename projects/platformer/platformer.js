564652db8c2964bedd6881ba519e0cd67dc68de0
denzall-dev brooks <denzall.brooks@nextlevelnola.org>
2024-09-09 20:34:24 +0000

    add collectible

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -54,6 +54,7 @@ $(function () {
     // Create collectables
     createCollectable("diamond", 500,400,20,0.5 )
     createCollectable("grace",500,300)
+    createCollectable("database",500,200,20,1)
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
@@ -64,8 +65,8 @@ $(function () {
     // TODO 3
     // Create cannons
     createCannon("right",100,1000)
-     createCannon("right",600,1000)
-    createCannon("bottom",600,900)
+     createCannon("right",600,2000)
+    createCannon("bottom",600,2000)
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
